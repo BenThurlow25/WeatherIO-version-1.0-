0891b86306165d0a42e0ee5b2cd0dfd88ab1a20a
Ben Thurlow <benthurlow@icloud.com>
2025-10-09 20:52:30 -0400

    fixing main errors

--- weather-frontend/src/App.jsx
+++ weather-frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
  const handleSubmit = async () => {
   setCurrentScreen('loading'); // ✅ show loading message
   try {
-    const response = await fetch(`http://localhost:3000/forecast?q=${location}`);
+    const response = await fetch(`https://weatherio-version-1-0.onrender.com/forecast?q=${location}`);
     const result = await response.json();
     console.log("📦 Full backend response:", result);
     console.log('Weekly:', result.dailyForecast);
